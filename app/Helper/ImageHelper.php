1a349f9887f414e8b1a88037a94efc84ea23e702
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-09-18 20:41:52 +0100

    image upload added

---
+++ app/Helper/ImageHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+class ImageHelper
+{
+    public static function save_image(UploadedFile $image, $uploadPath = 'uploads')
+    {
+        // Validate the image
+        $image->validate([
+            'image' => 'required|image|mimes:jpeg,jpg,png,gif|max:2048', // Adjust max size as needed
+        ]);
+
+        // Generate a unique file name
+        $fileName = uniqid() . '_' . time() . '.' . $image->getClientOriginalExtension();
+
+        // Store the image in the specified path
+        $imagePath = $image->storeAs($uploadPath, $fileName, 'public');
+
+        // Generate and return the image URL
+        $imageUrl = asset('storage/' . $imagePath);
+
+        return $imageUrl;
+    }
+}
