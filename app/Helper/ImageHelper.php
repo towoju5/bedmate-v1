13c487d8cd9da866c3615e7aef3af93a1f72a397
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-11-03 10:41:43 +0100

    major update, gallery,stories,chat

--- app/Helper/ImageHelper.php
+++ app/Helper/ImageHelper.php
@@ -9,30 +9,10 @@ class ImageHelper
 {
     public static function save_image(UploadedFile $image, $uploadPath = 'uploads')
     {
-<<<<<<< HEAD
         // Validate the image
         $image->validate([
             'image' => 'required|image|mimes:jpeg,jpg,png,gif|max:2048', // Adjust max size as needed
         ]);
-=======
-        $image->validate([
-            'image' => 'required|image|mimes:jpeg,jpg,png,gif|max:2048', // Adjust max size as needed
-        ]);
-
-        // Generate a unique file name
-        $fileName = uniqid() . '_' . time() . '.' . $image->getClientOriginalExtension();
-
-        // Store the image in the specified path
-        $imagePath = $image->storeAs($uploadPath, $fileName, 'public');
-
-        // Generate and return the image URL
-        $imageUrl = asset('storage/' . $imagePath);
-
-        return $imageUrl;
-        // $image->validate([
-        //     'image' => 'required|image|mimes:jpeg,jpg,png,gif|max:2048', // Adjust max size as needed
-        // ]);
->>>>>>> 8125e3ebae0a0c64387a90095e9faf4a8e98bd79
 
         // Generate a unique file name
         $fileName = uniqid() . '_' . time() . '.' . $image->getClientOriginalExtension();
