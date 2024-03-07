c96f7981aee38d02303a1acc030009d4efdb6b92
EMMANUEL TOWOJU <towojuads@gmail.com>
2024-03-07 18:20:27 +0100

    wallet management

---
+++ app/Http/Controllers/AccountCountroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountCountroller extends Controller
+{
+    public function get_account()
+    {
+        //
+    }
+}
